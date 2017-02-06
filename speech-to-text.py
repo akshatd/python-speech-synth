8aecc09b5faf64a7fa339eadab564b1fb415fe16
Akshat <akshatdubey@outlook.com>
2017-02-06 16:19:57 +0800

    added outputs, added google speech

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,5 +1,6 @@
 import speech_recognition
 import sys
+import time
 
 audio_filename = sys.argv[1]
 out_file = open('output.txt','w')
@@ -7,18 +8,43 @@ recognizer = speech_recognition.Recognizer()
 recognizer.energy_threshold = 150
 recognizer.dynamic_energy_threshold = True
 
+file_len = 2160
+chunk = 10
+wait = 5
 print "loading audio file: " + audio_filename
-with speech_recognition.AudioFile(audio_filename) as audio_file:
-    # recognizer.adjust_for_ambient_noise(audio_file,5)
-    for i in xrange(217):
-        audio = recognizer.record(audio_file, 10)
 
+with speech_recognition.AudioFile(audio_filename) as audio_file:
+    
+    audio = recognizer.record(audio_file, 300+420+180+420+240+60+300+60+120)
+    # recognizer.adjust_for_ambient_noise(audio_file,5)
+    for i in xrange(file_len/chunk):
+        audio = recognizer.record(audio_file, chunk)
+	    # Speech recognition using Google Speech Recognition
+        
         try:
-            text = str(i/6)+":"+str(10*(i%6))+" > "+ recognizer.recognize_sphinx(audio)
+	    # for testing purposes, we're just using the default API key
+	    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+	    # instead of `r.recognize_google(audio)`
+            text = str(i*chunk/60)+":"+str((i*chunk)%60)+" > "+ recognizer.recognize_google(audio)
+            print text
+            out_file.write(text+'\n')
+        except speech_recognition.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except speech_recognition.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        
+        if i*chunk%300 == 0:
+            time.sleep(30)
+
+out_file.close()
+
+"""
+        try:
+            text = str(i/6)+":"+str(10*(i%6))+" > "+ recognizer.recognize_google(audio)
             print text
             out_file.write(text+'\n')
         except speech_recognition.UnknownValueError:
             print("Sphinx could not understand audio")
         except speech_recognition.RequestError as e:
             print("Sphinx error; {0}".format(e))
-out_file.close()
+"""
